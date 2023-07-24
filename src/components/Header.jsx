9151c70dad8027cb0ca62ab0d45eeb7dab77ecf1
xkostolanskym <kostolansky.marek@gmail.com>
2023-07-24 16:30:17 +0200

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,30 @@
+import hero from "../images/hero-image.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section className="p-5">
+      <div className="container">
+        <div className="row align-items-center justify-content-between">
+          <div className="col-md">
+            <h1 className="display-5">
+              One haircut will not suit to everyone.
+            </h1>
+            <p>
+              Get your hair done by professional and experience something new
+              and exciting
+            </p>
+            <button className="btn btn bg-secondary mx-2 p-3">
+              Book a date
+            </button>
+            <button className="btn btn bg-dark mx-2 p-3">Explore more</button>
+          </div>
+          <div className="col-md">
+            <img className="img-fluid p-5" src={hero} alt="hero" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,55 @@
+import logo from "../images/logo.png";
+import { FaLinkedin, FaFacebook, FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div className="container">
-      <nav class="navbar navbar-expand-lg ">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
-          </a>
+    <div>
+      <nav class="navbar navbar-expand-lg text-center">
+        <div class="container">
+          <Link class="navbar-brand" exact to="/">
+            <img src={logo} />
+          </Link>
           <button
             class="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarText"
-            aria-controls="navbarText"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarText">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
+                <Link class="nav-link" to="/about">
+                  About
+                </Link>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
+                <Link class="nav-link" to="/contact">
+                  Contact
+                </Link>
               </li>
             </ul>
-            <span class="navbar-text">Navbar text with an inline element</span>
+            <a
+              class="nav-link fs-3 p-1 text-center"
+              href="https://www.linkedin.com/in/marek-kostolansky-762185206/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              class="nav-link p-1 fs-3 text-center"
+              href="https://www.facebook.com/profile.php?id=1617340063"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
           </div>
         </div>
       </nav>
